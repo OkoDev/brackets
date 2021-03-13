a1bca47383adf3067203ab3be769e69fc76addc3
okodev <oko.webdev@gmail.com>
2021-03-13 19:22:19 +0300

    feat: add solution to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const br = bracketsConfig.map(b => b.join(""));
+    for (let i = 0; i < br.length; i++) {
+        if (str.includes(br[i])) {
+            str = str.replace(br[i], "");
+            console.log(str);
+             i = -1 ;
+        }
+    }
+    return !str;
+};
+
